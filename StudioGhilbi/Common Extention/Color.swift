6cc5183c1ec5219f70207257133f17b77b88b73d
Alvin Matthew Pratama <alvinmatthew12@gmail.com>
2021-04-04 22:54:04 +0700

    Change random color from hue&saturation to RGBA

--- StudioGhilbi/Common Extention/Color.swift
+++ StudioGhilbi/Common Extention/Color.swift
@@ -9,6 +9,6 @@ import UIKit
 
 extension UIColor {
     static var random: UIColor {
-        return .init(hue: .random(in: 0...1), saturation: 1, brightness: 1, alpha: 1)
+        return .init(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1.0)
     }
 }
